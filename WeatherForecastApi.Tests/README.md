ef9199d8870e01c7dc3ff9999c2132fd1be323dc
jim-wei-wedbush <159960180+jim-wei-wedbush@users.noreply.github.com>
2024-06-04 16:38:52 -0500

    POC init

---
+++ WeatherForecastApi.Tests/README.md
@@ -0,0 +1,21 @@
+﻿# POC Simple API Test
+
+A simple API Test POC implementation using ASP.NET Core API, and a corresponding xUnit test project.
+
+## Notes:
+
+- Used default WeatherForecastController class
+- Added xUnit test project and API project reference
+- Created WeatherForecastControllerTests class, and test method Get_ReturnsWeatherForecasts(), using AAA
+
+### Arrange:
+- In test method, created mock instance of ILogger<WeatherForecastController>
+
+### Act:
+- Called GET method of WeatherForecastController, retrieved weather forecasts.
+
+### Assert:
+- Verified the result is:
+	1. Not null
+	2. Assignable from IEnumerable<WeatherForecast>
+	1. Not empty
---
+++ WeatherForecastApi.Tests/WeatherForecastControllerTests.cs
@@ -0,0 +1,35 @@
+﻿// WeatherForecastApi.Tests
+using POC_Simple_API_Test.Controllers;
+using POC_Simple_API_Test;
+using Moq;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherForecastApi.Tests
+{
+    public class WeatherForecastControllerTests
+    {
+
+        [Fact]
+        public void Get_ReturnsWeatherForecasts()
+        {
+            // AAA Pattern
+            // Arrange
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<IEnumerable<WeatherForecast>>(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
